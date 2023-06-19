fcb71668804c22a85833363c949dbea952519d27
Loic2dot0 <moreaux.loic@gmail.com>
2023-06-19 11:45:52 +0200

    update User

--- src/tpm2i/Model/User.java
+++ src/tpm2i/Model/User.java
@@ -10,4 +10,77 @@ package tpm2i.Model;
  */
 public class User {
     
+    private long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private String login;
+    private String password;
+    
+    //Constructor without parameters
+    public User(){
+        
+    }
+    
+    // Getters
+    public long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    // Setters
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+   
 }
