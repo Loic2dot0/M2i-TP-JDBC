a9ce4f7a60d798e5a4720dc28a76dc6825bff583
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-06-19 11:54:56 +0200

    Add Article & Provider models

---
+++ src/tpm2i/Model/Article.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpm2i.Model;
+
+/**
+ *
+ * @author N Duquesne
+ */
+public class Article {
+    private Long id;
+    private String idNumber;
+    private String name;
+    private String description;
+    private Boolean isForSell;
+    
+    public Article() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsForSell() {
+        return isForSell;
+    }
+
+    public void setIsForSell(Boolean isForSell) {
+        this.isForSell = isForSell;
+    }
+    
+    
+}
---
+++ src/tpm2i/Model/Provider.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpm2i.Model;
+
+/**
+ *
+ * @author N Duquesne
+ */
+public class Provider {
+    private Long id;
+    private String idNumber;
+    private String name;
+    private String email;
+    private String adress;
+    
+    public Provider(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    
+    
+    
+    
+}
